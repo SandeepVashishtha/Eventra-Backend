eda4f8e7be23460975405cbb61438da820cfcd9c
Samarpit <samarpitrawat97@gmail.com>
2026-05-22 21:57:43 +0530

    Fix syntax error in JwtTokenProvider

--- src/main/java/com/sandeep/eventrabackend/EventraBackendApplication.java
+++ src/main/java/com/sandeep/eventrabackend/EventraBackendApplication.java
@@ -2,8 +2,10 @@ package com.sandeep.eventrabackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling; // 1. Added import
 
 @SpringBootApplication
+@EnableScheduling // 2. Added annotation to turn on background cleanup
 public class EventraBackendApplication {
 
     public static void main(String[] args) {
